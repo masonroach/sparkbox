c3e1ff65c1c4e106611203234f6c1d1365dbfe94
Mason Roach <mason.roach@maine.edu>
2018-04-11 15:04:48 -0400

    Added a main function to test functionality

--- src/main.c
+++ src/main.c
@@ -1,39 +1,22 @@
 #include "stm32f3xx.h"
-//#include "usartDriver.h"
+#include "usartDriver.h"
+#include "ledDriver.h"
+#include "button.h"
 
 int main(void) {
-/*	int8_t i = 0;
-	int8_t *s = (int8_t *)" Testing . . .\n";
+	int8_t i = 0;
 
-	RCC->CR |= (RCC_CR_HSION);
-	
-	while ((RCC->CR & RCC_CR_HSIRDY) == 0);	// Wait for HSI to be ready
-	RCC->CFGR &= ~RCC_CFGR_SW;		// Select HSI as clock
-	RCC->CFGR |=  RCC_CFGR_SW_HSI;
-	while ((RCC->CFGR & RCC_CFGR_SWS) == 0);
-*/
-
-
-
-	RCC->AHBENR    |=   RCC_AHBENR_GPIOEEN;		// Enable GPIOE Clock
-	GPIOE->MODER   |=   GPIO_MODER_MODER8_0;	// GPIO Mode output
-	GPIOE->OTYPER  &=  ~GPIO_OTYPER_OT_8;		// GPIO OType OD
-	GPIOE->PUPDR   |=   GPIO_PUPDR_PUPDR8_1;	
-	GPIOE->OSPEEDR |=   GPIO_OSPEEDER_OSPEEDR8_0;	// GPIO Speed
-
-	GPIOE->BSRR    |=   GPIO_BSRR_BS_8;		// Turn on led
-
-
-
-
-/*
-	gpioConfig();
+	initLeds();
 	usartConfig();
-	usartSendChar('T');
+	initButton();	
+
 	while (1) {
-		usartSendString(s);
-		usartSendChar(i);
+		while (readButton() == 0);	// Wait while button is not pushed
+		if (++i > 8) i = 0;
+		ledCircle(i);
+		while (readButton() == 1);	// Wait while button is pushed
+		usartSendChar('0' + readButton());
 	}
-*/
+
 	return 1;
 }
