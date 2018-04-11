d86948e4eac18cdbf348e4a898b500c1a50b77c9
Mason Roach <mason.roach@maine.edu>
2018-04-11 15:03:44 -0400

    Added a driver to interface with the user button on the discovery board

---
+++ inc/button.h
@@ -0,0 +1,4 @@
+#include "stm32f3xx.h"
+
+void initButton(void);
+uint8_t readButton(void);
---
+++ src/button.c
@@ -0,0 +1,19 @@
+#include "button.h"
+
+// Initialize the button (PA0) as an input
+void initButton(void) {
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;	// Enable GPIOA Clock
+	
+	GPIOA->MODER &= ~GPIO_MODER_MODER0;	// GPIO Mode Input
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT_0;	// GPIO OType -> Push-Pull
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR0_1;	// GPIO PU/PD -> Pull-down
+}
+
+// Read the value of the button
+uint8_t readButton(void) {
+	if (GPIOA->IDR & GPIO_IDR_0) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
