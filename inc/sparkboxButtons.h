eebb8df393a17b6cd51d506414192e1fea98605d
Patrick Roy <patrick.roy1@maine.edu>
2018-11-15 15:38:53 -0500

    Updated sparkbox button code for new PCB, verified

--- inc/sparkboxButtons.h
+++ inc/sparkboxButtons.h
@@ -9,14 +9,14 @@ extern volatile uint8_t buttons;
 
 #define ALL_BUTTONS (buttons)
 
-#define BUTTON_LEFT ((buttons >> 0) & 0x01)
-#define BUTTON_RIGHT ((buttons >> 1) & 0x01)
-#define BUTTON_UP ((buttons >> 2) & 0x01)
-#define BUTTON_DOWN ((buttons >> 3) & 0x01)
-#define BUTTON_A ((buttons >> 4) & 0x01)
-#define BUTTON_B ((buttons >> 5) & 0x01)
-#define BUTTON_X ((buttons >> 6) & 0x01)
-#define BUTTON_Y ((buttons >> 7) & 0x01)
+#define BUTTON_A 		((buttons >> 0) & 0x01)
+#define BUTTON_B 		((buttons >> 1) & 0x01)
+#define BUTTON_X 		((buttons >> 2) & 0x01)
+#define BUTTON_Y 		((buttons >> 3) & 0x01)
+#define BUTTON_DOWN 	((buttons >> 4) & 0x01)
+#define BUTTON_UP 		((buttons >> 5) & 0x01)
+#define BUTTON_RIGHT 	((buttons >> 6) & 0x01)
+#define BUTTON_LEFT 	((buttons >> 7) & 0x01)
 
 #define PUSHED 0x01
 #define RELEASED 0x00
