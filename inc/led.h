f9e59035f149a0bacecabdd7879d4eab32f166c7
masonroach <mason.roach@maine.edu>
2018-12-12 14:09:23 -0500

    Started adding doxygen doccumentation, starting with led.[ch]

--- inc/led.h
+++ inc/led.h
@@ -1,21 +1,86 @@
+/*! 
+ * @file led.h
+ * @author Mason Roach
+ * @author Patrick Roy
+ * @date Dec 12 2018
+ *
+ * @brief Functions the control the LEDs on the Sparkbox device
+ *
+ * These functions should be used to cotrol the 8 general purpose LEDs on the
+ * Sparkbox, as well as the two debugging LEDs.
+ */
+
 #include "stm32f4xx.h"
 
 #ifndef SPARK_LED
 #define SPARK_LED
 
-// LED functions for the sparkbox, not the discovery
+/*!
+ * @brief Enumerable used with ledError function
+ */
 typedef enum {
-	LED_OFF		= 0,
-	LED_WARNING	= 1,
-	LED_ERROR	= 2
+	LED_OFF		= 0,	/*!< Turns both LEDs off */
+	LED_WARNING	= 1,	/*!< Controls the yellow LED */
+	LED_ERROR	= 2		/*!< Controls the red LED */
 } LED_DEBUG;
 
+/*!
+ * @brief Initializes LED hardware
+ *
+ * Initializes the proper registers on the STM32F4 to be outputs
+ */
 void initLeds(void);
+
+/*!
+ * @brief Turns on a desginated LED
+ *
+ * @param led Designates which LED to turn on
+ */
 void ledOn(uint8_t led);
+
+/*!
+ * @brief Turn off a designated LED
+ *
+ * @param led Designates which LED to turn off
+ */
 void ledOff(uint8_t led);
+
+/*!
+ * @brief Toggle a designated LED
+ *
+ * @param led Designates which LED to turn toggle
+ */
 void ledToggle(uint8_t led);
+
+/*!
+ * @brief Turn all general purpose LEDs on
+ */
 void ledAllOn(void);
+
+/*!
+ * @brief Turn all general purpose LEDs off
+ */
 void ledAllOff(void);
+
+/*!
+ * @brief Set the error LED set
+ *
+ * The error LED set has three possible states: both off, yellow on, or red on.
+ * The function will set the state of these LEDs to the given input state.
+ * @see LED_DEBUG
+ *
+ * @param status Designate which state the LED set should be in
+ */
 void ledError(LED_DEBUG status);
-uint8_t ledMap(uint8_t map);
+
+/*!
+ * @brief Input a 8-bit map to turn the corresponding LED's on and off
+ *
+ * The input value will be used as a bitmap to enable or disable the general
+ * purpose LEDs on the Sparkbox. Values of 1 will turn on the LED, values of 0
+ * will turn off the LED.
+ *
+ * @param map The bitmap to push to the LEDs
+ */
+void ledMap(uint8_t map);
 #endif
--- src/led.c
+++ src/led.c
@@ -1,5 +1,22 @@
+/*!
+ * @file led.c
+ * @author Mason Roach
+ * @author Patrick Roy
+ * @date Dec 12 2018
+ *
+ * @brief Functions the control the LEDs on the Sparkbox device
+ *
+ * These functions should be used to cotrol the 8 general purpose LEDs on the
+ * Sparkbox, as well as the two debugging LEDs.
+ */
+
 #include "led.h"
 
+/*!
+ * @brief Initializes LED hardware
+ *
+ * Initializes the proper registers on the STM32F4 to be outputs
+ */
 void initLeds(void) {
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN |	// Enable GPIOC Clock
 					RCC_AHB1ENR_GPIOBEN;	// Enable GPIOB Clock
@@ -10,32 +27,56 @@ void initLeds(void) {
 	GPIOB->OTYPER &= ~GPIO_OTYPER_OT_0;		// Open drain
 }
 
-// Turns on a desginated LED
+/*!
+ * @brief Turns on a desginated LED
+ *
+ * @param led Designates which LED to turn on
+ */
 void ledOn(uint8_t led) {
 	GPIOC->ODR |= 1 << led;	// Turn on led
 }
 
-// Turn off a designated LED
+/*!
+ * @brief Turn off a designated LED
+ *
+ * @param led Designates which LED to turn off
+ */
 void ledOff(uint8_t led) {
 	GPIOC->ODR &= ~(1 << led);	// Turn off led
 }
 
-// Toggle a designated LED
+/*!
+ * @brief Toggle a designated LED
+ *
+ * @param led Designates which LED to turn toggle
+ */
 void ledToggle(uint8_t led) {
 	GPIOC->ODR ^= 1 << led;
 }
 
-// Turn all LEDs on
+/*!
+ * @brief Turn all general purpose LEDs on
+ */
 void ledAllOn(void) {
 	GPIOC->ODR |= 0x00FF;
 }
 
-// Turn all LEDs off
+/*!
+ * @brief Turn all general purpose LEDs off
+ */
 void ledAllOff(void) {
 	GPIOC->ODR &= 0xFF00;
 }
 
-// Sets the error led off (0), yellow (1), or red (2)
+/*!
+ * @brief Set the error LED set
+ *
+ * The error LED set has three possible states: both off, yellow on, or red on.
+ * The function will set the state of these LEDs to the given input state.
+ * @see LED_DEBUG
+ *
+ * @param status Designate which state the LED set should be in
+ */
 void ledError(LED_DEBUG status) {
 	if (status) {	// Enabled, either yellow or red
 		GPIOB->MODER |= GPIO_MODER_MODE0_0;	// Set pin as output
@@ -49,9 +90,15 @@ void ledError(LED_DEBUG status) {
 	}
 }
 
-// Input a 8-bit map to turn the corresponding LED's on and off
-uint8_t ledMap(uint8_t map) {
+/*!
+ * @brief Input a 8-bit map to turn the corresponding LED's on and off
+ *
+ * The input value will be used as a bitmap to enable or disable the general
+ * purpose LEDs on the Sparkbox. Values of 1 will turn on the LED, values of 0
+ * will turn off the LED.
+ *
+ * @param map The bitmap to push to the LEDs
+ */
+void ledMap(uint8_t map) {
 	GPIOC->BSRR |= (map & 0xFF) + ((~map & 0xFF) << 16);
-
-	return map;
 }
